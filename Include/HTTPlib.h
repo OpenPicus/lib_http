ecd3b6d8cea899c02d4d9745052f342251d774a0
stefacc <stefano.saccucci@gmail.com>
2014-02-26 17:54:09 +0100

    first commit

---
+++ Include/HTTPlib.h
@@ -0,0 +1,27 @@
+/** \file HTTPlib.h
+ *  \brief HTTP library
+ */
+
+/**
+\mainpage Main page
+
+ Library to manage HTTP requests.
+*/
+
+#include "taskFlyport.h"
+
+#define ARRAY_SIZE(x) (sizeof(x))
+
+/*****************************************************************************
+        MQTT function declarations
+*****************************************************************************/
+
+int HTTP_GET(TCP_SOCKET socket, char * host, char * path_data, char * custom_header, char * header, int headersize, char * body, int bodysize, int timeout);
+int HTTP_GETsimple(TCP_SOCKET socket, char * host, char * path_data, char * header, int headersize, char * body, int bodysize);
+int HTTP_POST(TCP_SOCKET socket, char * host, char * path, char * custom_header, char * CType, char * data, char * header, int headersize, char * body, int bodysize, int timeout);
+int HTTP_POSTsimple(TCP_SOCKET socket, char * host, char * path, char * data, char * header, int headersize, char * body, int bodysize);
+void HTTP_URLEncode(char * dest, char * src);
+int HTTP_URLEncodeLen(char * str);
+void HTTP_URLDecode(char * dest, char * src);
+int HTTP_URLDecodeLen(char * str);
+int HTTP_Read(TCP_SOCKET socket, char * header, int headersize, char * body, int bodysize, int timeout);
--- README.md
+++ README.md
@@ -2,3 +2,10 @@ lib_http
 ========
 
 openpicus flyport - library for http requests
+
+Flyport library for HTTP requests, released under GPL v.3.
+The library allows you to use HTTP requests
+More info on http://wiki.openpicus.com and download it http://wiki.openpicus.com/index.php/HTTP.
+
+1) import files inside Flyport IDE using the external libs button.
+2) add following code example in FlyportTask.c:
